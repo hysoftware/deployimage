89b80e00fa792a56e7058c23f8ce5941a804bdb8
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-06-12 13:50:17 +0900

    added lazy-apps

--- webapp/runserver.sh
+++ webapp/runserver.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
 secret=$(mkpasswd -l 40 -s 4 -C 12 -c 12 -d 12) \
-repo=${HOME}/venv/webapp uwsgi -y /home/hysoft/venv/webapp/etc/uwsgi.yml
+repo=${HOME}/venv/webapp uwsgi --lazy-apps \
+-y /home/hysoft/venv/webapp/etc/uwsgi.yml
